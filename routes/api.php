6a77f23436835d7db10a5ec47a30d52a78fbf00b
AliAlhamoli <alialhamoli475@gmail.com>
2024-12-08 14:53:05 +0200

    fix namespace to AuthController in API directory

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
